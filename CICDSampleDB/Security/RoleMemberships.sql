93e08cbe735313e2c31af5016889de41be494088
Aditya Chouksey <adi.choukse@gmail.com>
2022-07-20 17:12:40 +0200

    CICDSample Project Added

---
+++ CICDSampleDB/Security/RoleMemberships.sql
@@ -0,0 +1,18 @@
+﻿ALTER ROLE [db_owner] ADD MEMBER [SQLforSSAS];
+
+
+GO
+ALTER ROLE [db_accessadmin] ADD MEMBER [SQLforSSAS];
+
+
+GO
+ALTER ROLE [db_securityadmin] ADD MEMBER [SQLforSSAS];
+
+
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [SQLforSSAS];
+
+
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [SQLforSSAS];
+
