2f06f88324ffe8b8ae21334ccc737c13145401a1
Aditya Chouksey <adi.choukse@gmail.com>
2022-08-14 19:40:42 +0200

    brand category added

--- CICDSampleDB/production/Tables/brands.sql
+++ CICDSampleDB/production/Tables/brands.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [production].[brands] (
     [brand_id]   INT           IDENTITY (1, 1) NOT NULL,
     [brand_name] VARCHAR (255) NOT NULL,
+    [brand cat] INT NULL, 
     PRIMARY KEY CLUSTERED ([brand_id] ASC)
 );
 
